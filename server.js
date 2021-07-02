1943bb80dec71cbfa65fed888ad00beb2a72bc64
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2021-07-02 09:43:15 -0700

    add starter code

---
+++ server.js
@@ -0,0 +1,18 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+// Routes
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Hello world');
+});
